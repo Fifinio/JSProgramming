01ff7cd0bc43c558b437f6dbbee8fde44ce141f3
Fifinio <fiifnio13@gmail.com>
2022-10-30 14:45:15 +0100

    slider dasięludziompokazać

--- Lab 2 - slider/index.js
+++ Lab 2 - slider/index.js
@@ -1,20 +1,34 @@
 const wrapper = document.querySelector(".wrapper")
 const carouselContainer = document.querySelector(".carousel-container")
 const carousel = document.querySelector(".carousel")
-
 const slides = carousel.querySelectorAll(".slide")
+const indexButtons = document.querySelectorAll(".idx-btn")
+
 var currentSlideIdx = 1;
+
 var size = 600;
 var isHovering = false;
 
+const slideTo = (btn,index) =>{
+  indexButtons.forEach((btn) => btn.classList.remove("highlighted"))
+  btn.classList.toggle("highlighted")
+  currentSlideIdx = (index) % slides.length;
+  carousel.style.transform = `translateX(${-size * currentSlideIdx}px)`
+}
+
 const slideRight = () => {
+  indexButtons.forEach((btn) => btn.classList.remove("highlighted"))
+  indexButtons[currentSlideIdx].classList.toggle("highlighted")
   carousel.style.transform = `translateX(${-size * currentSlideIdx}px)`
   currentSlideIdx = (currentSlideIdx + 1) % slides.length;
+
 }
 const slideLeft = () => {
   // weirdo
+  indexButtons.forEach((btn) => btn.classList.remove("highlighted"))
   if(currentSlideIdx === 1) currentSlideIdx = slides.length-1
   else currentSlideIdx = (currentSlideIdx - 1) % slides.length 
+  indexButtons[currentSlideIdx].classList.toggle("highlighted")
   carousel.style.transform = `translateX(${-size*currentSlideIdx}px)`
 
 }   
@@ -25,3 +39,7 @@ wrapper.querySelector('.btn.right').addEventListener('click', slideRight)
 wrapper.addEventListener('mouseover', () => isHovering = true)
 wrapper.addEventListener('mouseout', () => isHovering = false)
 
+indexButtons.forEach((idxBtn, idx) => {
+  console.log(idxBtn, idx)
+  idxBtn.addEventListener('click', () => slideTo(idxBtn, idx))
+})
